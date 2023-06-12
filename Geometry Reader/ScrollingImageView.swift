4e698fae0e42c8b130fdd0680bd4aaa3b9ab8503
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-06-13 00:10:41 +0200

    feat: Example 2

--- Geometry Reader/ScrollingImageView.swift
+++ Geometry Reader/ScrollingImageView.swift
@@ -16,12 +16,24 @@ struct ScrollingImageView: View {
           Array(1...7).map {"PalmSprings - \($0)"}
       }
       var body: some View {
-          ScrollView(.horizontal) {
-              HStack(spacing: 0) {
-                  ForEach(imageNames, id: \.self) { imageName in
-                      Image(imageName)
-                          .resizable()
-                          .scaledToFill()
+          GeometryReader { screen in
+              ScrollView(.horizontal) {
+                  HStack(spacing: 0) {
+                      ForEach(imageNames, id: \.self) { imageName in
+                          GeometryReader { geometry in
+                              Image(imageName)
+                                  .resizable()
+                                  .scaledToFill()
+                                  .frame(width: screen.size.width)
+                                  .offset(x: -geometry.frame(in: .global).origin.x)
+                                  .clipped()
+//                              Text(geometry.xOrigin(space: .global))
+//                                  .font(.largeTitle)
+//                                  .foregroundColor(.white)
+//                                  .padding(100)
+                          }
+                          .frame(width: screen.size.width)
+                      }
                   }
               }
           }
